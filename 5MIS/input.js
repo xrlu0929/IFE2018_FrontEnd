6f37f81cb2a7378d0f788ecbdcca6466e739537e
xrlu0929 <xl2474@tc.columbia.edu>
2018-12-11 01:17:35 -0500

    input change

---
+++ 5MIS/input.js
@@ -0,0 +1,40 @@
+function edit(){
+   let table = document.getElementById("table");
+    console.log("table: ", table)
+    let tds = table.querySelectorAll("td");
+    console.log("tds: ", tds)
+    for (let i = 0; i < tds.length; i++) {
+        let td = tds[i];
+        
+        td.addEventListener("mouseenter", function (e) {
+            console.log(td)
+            let oEvent = e || event;
+            let target = e.target || e.srcElement;
+            console.log("target: ", target)
+            
+            var img = document.createElement("img");
+            img.src = "edit.png";
+            img.className="img";
+            target.appendChild(img)
+//            target.removeChild(img)
+
+        });
+        
+        td.addEventListener("mouseleave", function (e) {
+            let target = e.target || e.srcElement;
+            var img = td.querySelectorAll("img")[0];
+            console.log(img)
+            target.removeChild(img)
+
+        });
+        
+        td.addEventListener("click", (e)=>{
+            let target = e.target || e.srcElement;
+            let input =  document.createElement("input");
+            target.appendChild(input)
+        })
+        
+
+    }
+    
+}
