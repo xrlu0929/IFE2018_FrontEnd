f187722322ca6d9d7f7496a62b90a46dba8ce312
xrlu0929 <xl2474@tc.columbia.edu>
2018-12-28 11:16:59 -0500

    prototype and call

--- 6restaurant/oop.js
+++ 6restaurant/oop.js
@@ -1,8 +1,51 @@
-function Restaurant() {
-  var obj = {};
-  obj. = name;
-  obj.greeting = function () {
-    alert('Hi! I\'m ' + this.name + '.');
-  }
-  return obj;
+function Restaurant(input) {
+	/**
+	 * two ways to find it
+	 */
+// 	let obj = {}
+// 	obj.cash = input.cash;
+// 	obj.seats = input.seats;
+// 	obj.staff = input.staff;
+// 	return obj;
+	this.cash = input.cash;
+	this.seats = input.seats;
+	this.staff = input.staff;
+	// this.hire()
 }
+Restaurant.prototype.hire = function(e){
+	this.staff.push(e)
+	this.cash = this.cash - e.salary
+}
+
+function Cook(name, salary){
+	this.name = name;
+	this.salary = salary;
+}
+
+// make a waiter class
+function Waiter(name, salary, experienced){
+	Cook.call(this, name, salary)
+	
+	this.experienced = experienced
+	console.log(this)
+}
+
+
+var ifeRestaurant = new Restaurant({
+    cash: 1000000,
+    seats: 20,
+    staff: []
+});
+console.log(ifeRestaurant)
+
+var newCook = new Cook("Tony", 10000);
+var cook1 = new Cook("Yunlei", 90000)
+
+var waiter0 = new Waiter('Taoer', 10000, true);
+
+ifeRestaurant.hire(newCook);
+ifeRestaurant.hire(cook1);
+ifeRestaurant.hire(waiter0);
+
+
+console.log(ifeRestaurant)
